f459e84a27e0838db6d78f3baf9e5a9563f5b94a
domotova <domot558@gmail.com>
2019-09-28 13:09:29 +0900

    hw

---
+++ Lab1/Zad1.FizzBuzz.py
@@ -0,0 +1,10 @@
+def fizzbuzz(n):
+    sum = 0
+    for i in range(n):
+        if i%3 == 0 or i%5 == 0:
+            sum = sum+i
+    return sum
+
+
+if __name__ == '__main__':
+    print(fizzbuzz(1001))
